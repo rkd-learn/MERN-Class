e24dc4743671225397f9df6f317126cf41d2b121
razzkumar <razzkumar.dev@gmail.com>
2022-07-21 20:21:12 +0545

    Add Day4 api codebase

---
+++ Day4-API/README.md
@@ -0,0 +1,17 @@
+# API
+
+REST API
+
+JSON
+
+GET request for JSON
+
+Parsing the JSON
+
+Getting live API data
+
+Middleware
+
+Body parser to post request to the server
+
+Week 2 Assignment
---
+++ Day4-API/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json())
+
+function logger(req,res,next){
+    const url = req.url;
+    const method = req.method;
+    console.log(url,method)
+
+    next()
+}
+
+app.use(logger)
+
+
+//REST API
+//XML SOAP
+
+const users = [
+  {
+    id:1,
+    name: "raj",
+    address: "New Baneshwor",
+    age: 28,
+  },
+  {
+    id:2,
+    name: "kumar",
+    address: "New Baneshwor",
+    age: 29,
+  },
+  {
+    id:3,
+    name: "dhiraj",
+    address: "USA",
+    age: 28,
+  },
+];
+
+app.get("/users",(req, res) => {
+  res.json(users);
+});
+
+app.get("/user/:id",(req,res)=>{
+
+    const id = req.params.id;
+
+    const currentUser = users.find(a=>a.id === Number(id))
+
+    if(currentUser){
+        res.json(currentUser)
+        return
+    }
+    res.status(404).send("User not found")
+})
+
+app.post("/user",(req,res)=>{
+    
+    users.push(req.body)
+
+    res.json(req.body)
+})
+
+app.listen(3001, () => {
+  console.log("Server is listening on port 3001");
+});
